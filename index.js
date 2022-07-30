875689f57cc1cbb845fdff5ea65052f37baa0ed4
muhdyusuf <92169759+muhdyusuf@users.noreply.github.com>
2022-07-30 23:02:52 +0800

    fcc last project

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+// multer setup
+const multer =require("multer")
+
+const storage = multer.diskStorage({
+  destination:(req,file,cb)=>{
+    cb(null,"upfile")
+  },
+  filename:(req,file,cb)=>{
+    cb(null,file.originalname)
+  }
+
+
+})
+const upload = multer({storage:storage}) 
+
 var app = express();
 
 app.use(cors());
@@ -10,6 +25,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse',upload.single("upfile"),(req,res)=>{
+ console.log(req.file)
+  
+  res.send({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size
+  })
+
+})
 
 
 
